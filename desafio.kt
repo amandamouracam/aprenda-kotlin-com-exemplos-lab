725c8bc540aa14211f8bc2b092617b49d0d14d47
amandamouracam <96317734+amandamouracam@users.noreply.github.com>
2023-09-27 15:51:04 -0300

    resolução desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,49 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario (val nomeUsuario: String)
 
-class Usuario
+data class ConteudoEducacional(val nomeConteudo: String, var duracao: Int = 60)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nomeFormacao: String, val nivel: Nivel, var conteudos: MutableList<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        
+        if(!inscritos.contains(usuario)){
+            inscritos.add(usuario)
+            println("Usuário ${usuario.nomeUsuario} matriculado na formação $nomeFormacao de nivel $nivel")
+        }
+        else{
+            println("Usuário ${usuario.nomeUsuario} já está matriculado na formação $nomeFormacao de nivel $nivel")
+        }
+        
     }
+    
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    //criar conteudos e formação
+    
+    val conteudo1 = ConteudoEducacional("Conhecendo a Linguagem Kotlin", 2)
+    val conteudo2 = ConteudoEducacional("Estruturas de dados em Kotlin", 4)
+
+    val formacao = Formacao("Desenvolvedor Kotlin", Nivel.INTERMEDIARIO, mutableListOf(conteudo1, conteudo2))
+    
+    //criar usuarios
+    
+    val usuario1 = Usuario("Alice")
+    val usuario2 = Usuario("João")
+    
+    //matricular usuarios
+    
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    formacao.matricular(usuario2) //tentativa de matricular o usuario2 novamente
+    
+    //checar inscritos
+    
+    println("Inscritos na formação ${formacao.nomeFormacao}: ${formacao.inscritos.map { it.nomeUsuario }}")
+
 }
